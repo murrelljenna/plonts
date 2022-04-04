433ec0a7239d2541745f713452eafd7a5f537bb6
Jenna Murrell <murrelljenna@gmail.com>
2022-04-03 21:22:37 -0400

    Spawn bouncy ball

--- Assets/Scripts/Networking/NetworkSpawner.cs
+++ Assets/Scripts/Networking/NetworkSpawner.cs
@@ -18,12 +18,14 @@ public class NetworkSpawner : MonoBehaviour, INetworkRunnerCallbacks
     private NetworkRunner _runner;
 
     [SerializeField] private NetworkPrefabRef _playerPrefab;
+    [SerializeField] private NetworkPrefabRef _ballPrefab;
     private Dictionary<PlayerRef, NetworkObject> _spawnedCharacters = new Dictionary<PlayerRef, NetworkObject>();
 
     public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
     {
         // Create a unique position for the player
         Vector3 spawnPosition = new Vector3((player.RawEncoded % runner.Config.Simulation.DefaultPlayers) * 3, 1, 0);
+        runner.Spawn(_ballPrefab, new Vector3(0, 0, 0), Quaternion.identity, player);
         NetworkObject networkPlayerObject = runner.Spawn(_playerPrefab, spawnPosition, Quaternion.identity, player);
         // Keep track of the player avatars so we can remove it when they disconnect
         _spawnedCharacters.Add(player, networkPlayerObject);
