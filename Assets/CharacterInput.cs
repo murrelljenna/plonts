8b32a188084d15a72abb2975477fdcdf0d5d1a67
Jenna Murrell <murrelljenna@gmail.com>
2022-04-10 11:58:04 -0400

    Add mouseLook to FPS character

--- Assets/CharacterInput.cs
+++ Assets/CharacterInput.cs
@@ -1,17 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 [RequireComponent(typeof(NetworkCharacterControllerPrototype))]
 public class CharacterInput : MonoBehaviour
 {
+    [SerializeField] private MouseLook m_MouseLook;
     private NetworkCharacterControllerPrototype character;
+    private Camera m_Camera;
 
     void Start()
     {
         character = GetComponent<NetworkCharacterControllerPrototype>();
+        m_Camera = Camera.main;
+        m_MouseLook.Init(transform, m_Camera.transform);
     }
 
+    private void RotateView()
+    {
+        m_MouseLook.LookRotation(transform, m_Camera.transform);
+    }
+
+    private void Update()
+    {
+        RotateView();
+    }
+    
     void FixedUpdate()
     {
         if (Input.GetKeyDown(KeyCode.Space))
