6e149642d62f18666af2e01e576e735ecec696f7
Jenna Murrell <murrelljenna@gmail.com>
2022-08-04 19:35:47 -0400

    Pickup item and bring it with you

---
+++ Assets/PickupItem.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class PickupItem : MonoBehaviour
+{
+    private Camera camera;
+
+    private GameObject pickedUp;
+    private Transform target;
+
+    private void Start()
+    {
+        camera = GetComponent<Camera>();
+        target = transform.Find("PickupLocation");
+    }
+
+    private void Update()
+    {
+        Ray ray = camera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        RaycastHit hit;
+
+        if (Input.GetKeyDown(KeyCode.E))
+            if (pickedUp == null)
+            {
+                if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+                {
+                    var hitObject = hit.collider.gameObject.GetComponent<Pickupable>();
+                    if (hitObject != null)
+                    {
+                        pickedUp = hitObject.gameObject;
+                    }
+                }
+            }
+            else
+            {
+                pickedUp = null;
+            }
+
+        if (pickedUp != null)
+        {
+            pickedUp.transform.position = target.position;
+            pickedUp.transform.rotation = target.rotation;
+        }
+    }
+}
