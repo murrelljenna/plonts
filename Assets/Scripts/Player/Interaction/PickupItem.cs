b72583391181a7a3d7111b69070c3dc5d5e31e1c
Wyatt Carss <carss.w@gmail.com>
2022-08-15 19:29:36 -0400

    Add and use Storage, Holdable, Serializable.
    Now the player will catch an object that's thrown at them.

--- Assets/Scripts/Player/Interaction/PickupItem.cs
+++ Assets/Scripts/Player/Interaction/PickupItem.cs
@@ -11,60 +11,89 @@ public class PickupItem : NetworkBehaviour
 
     private Pickupable pickedUp;
     private Transform target;
+    private Storage storage;
+    public bool itemHeld;
 
     private bool cooledOff = true;
 
     private void Start()
     {
         player = transform.parent.gameObject;
-        target = transform.Find("PickupLocation");
+        target = transform.Find("HandsLocation");
         lookingAt = new List<Collider>();
+        storage = new Storage();
+        itemHeld = false;
     }
 
     public override void FixedUpdateNetwork()
     {
         if (GetInput(out NetworkInputPrototype input))
         {
-            if (input.ePressed)
+            if (input.ePressed && cooledOff)
             {
-                if (cooledOff)
+                if (!itemHeld)
                 {
-                    if (pickedUp == null)
-                        pickedUp = closestCollider(lookingAt)?.GetComponent<Pickupable>();
-                    else
-                        pickedUp = null;
-
-                    StartCoroutine(nameof(pickupCooloff));
+                    add(closestCollider(lookingAt)?.GetComponent<Pickupable>());
                 }
+                else
+                {
+                    drop();
+                }
+
+                StartCoroutine(nameof(pickupCooloff));
             }
 
-            if (input.IsDown(NetworkInputPrototype.BUTTON_FIRE))
+            if (itemHeld && input.IsDown(NetworkInputPrototype.BUTTON_FIRE))
             {
-                if (pickedUp != null)
-                {
-                    pickedUp.Throw(player.transform);
-                    pickedUp = null;
-                }
+                pickedUp.Throw(player.transform);
+                drop();
             }
         }
 
-        if (pickedUp != null)
+        if (itemHeld)
         {
             pickedUp.transform.position = target.position;
             pickedUp.transform.rotation = target.rotation;
         }
     }
 
+    public void add(Pickupable obj)
+    {
+        if (obj && !storage.hasItem())
+        {
+            storage.add(obj.GetComponent<Serializable>().description);
+            pickedUp = obj;
+            itemHeld = true;
+        }
+    }
+
+    public void drop()
+    {
+        if (storage.hasItem())
+        {
+            storage.clear();
+        }
+        if (pickedUp != null)
+        {
+            pickedUp = null;
+        }
+        itemHeld = false;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<Pickupable>())
+        {
             lookingAt.Add(other);
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.GetComponent<Pickupable>())
+        {
             lookingAt.Remove(other);
+        }
     }
 
     private Collider closestCollider(List<Collider> colliders) {
