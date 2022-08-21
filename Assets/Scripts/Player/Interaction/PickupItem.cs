87babac102c101d6865759741aa907b3f9983286
Jenna Murrell <murrelljenna@gmail.com>
2022-08-21 12:48:32 -0400

    Lerp item in hand instead of teleport
    This produces a nicer, more fluid movement of the item a player is
    holding.
    
    I also want this to eventually take into account collisions, so that you
    can pick up an item and put it through other items.

--- Assets/Scripts/Player/Interaction/PickupItem.cs
+++ Assets/Scripts/Player/Interaction/PickupItem.cs
@@ -12,9 +12,12 @@ public class PickupItem : NetworkBehaviour
     private Pickupable pickedUp;
     private Transform target;
     private Storage storage;
+    private float startTime;
+
     public bool itemHeld;
 
     private bool cooledOff = true;
+    const float speed = 1.0F;
 
     private void Start()
     {
@@ -34,6 +37,7 @@ public class PickupItem : NetworkBehaviour
                 if (!itemHeld)
                 {
                     add(closestCollider(lookingAt)?.GetComponent<Pickupable>());
+                    startTime = Time.time;
                 }
                 else
                 {
@@ -48,11 +52,28 @@ public class PickupItem : NetworkBehaviour
                 pickedUp.Throw(player.transform);
                 drop();
             }
-        }
 
+            lerpItemHeld();
+        }
+    }
+
+    private void lerpItemHeld()
+    {
         if (itemHeld)
         {
-            pickedUp.transform.position = target.position;
+            float journeyLength = Vector3.Distance(pickedUp.transform.position, target.position);
+
+            // Distance moved equals elapsed time times speed..
+            float distCovered = (Time.time - startTime) * speed;
+
+            // Fraction of journey completed equals current distance divided by total distance.
+            float fractionOfJourney = distCovered / journeyLength;
+
+            // Set our position as a fraction of the distance between the markers.
+            pickedUp.transform.position = Vector3.Lerp(pickedUp.transform.position, target.position, fractionOfJourney);
+
+            Debug.Log(pickedUp.transform.position);
+
             pickedUp.transform.rotation = target.rotation;
         }
     }
